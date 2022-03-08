6253bed58011a50fe91652ed26cd8081cfca49fe
tanimon <8575113+tanimon@users.noreply.github.com>
2022-03-08 10:16:51 +0900

    Add resources of a simple chatbot

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,16 +1,67 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lex from 'aws-cdk-lib/aws-lex';
 
 export class BackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lexRole = new iam.Role(this, 'LexRole', {
+      assumedBy: new iam.ServicePrincipal('lex.amazonaws.com'),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const maxPromptRetriesCount: number = 3;
+
+    const dateOfBirthSlot: lex.CfnBot.SlotProperty = {
+      name: 'DateOfBirth',
+      slotTypeName: 'AMAZON.Date',
+      valueElicitationSetting: {
+        slotConstraint: 'Required',
+        promptSpecification: {
+          maxRetries: maxPromptRetriesCount,
+          messageGroupsList: [
+            {
+              message: {
+                plainTextMessage: {
+                  value: '生年月日を教えてください。',
+                },
+              },
+            },
+          ],
+        },
+      },
+    };
+
+    const signUpIntent: lex.CfnBot.IntentProperty = {
+      name: 'SignUp',
+      sampleUtterances: [
+        {
+          utterance: '入会',
+        },
+      ],
+      slots: [dateOfBirthSlot],
+    };
+
+    const fallbackIntent: lex.CfnBot.IntentProperty = {
+      name: 'FallbackIntent',
+      parentIntentSignature: 'AMAZON.FallbackIntent',
+    };
+
+    const bot = new lex.CfnBot(this, 'SignUpBot', {
+      name: 'SignUpBot',
+      idleSessionTtlInSeconds: 5 * 60,
+      roleArn: lexRole.roleArn,
+      dataPrivacy: {
+        ChildDirected: true,
+      },
+      botLocales: [
+        {
+          localeId: 'ja_JP',
+          nluConfidenceThreshold: 0.4,
+          intents: [signUpIntent, fallbackIntent],
+        },
+      ],
+    });
   }
 }
